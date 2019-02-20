c1f8c0b37884642f44347dd26f03839b59be855e
Julian Napolitan <julian.napolitan@gmail.com>
2019-02-19 21:13:50 -0800

    Output links to json file

--- webscraper.py
+++ webscraper.py
@@ -1,18 +1,20 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+import json
 
-url = 'https://en.wikipedia.org/wiki/RMS_Titanic'
+url = 'https://en.wikipedia.org/wiki/Python_(programming_language)'
 response = requests.get(url, timeout=5)
 content = BeautifulSoup(response.content, 'html.parser')
 text = content.find('div', attrs={"class": "mw-parser-output"})
 links = []
 
 for link in text.findAll('a', attrs={'href': re.compile("^/wiki/")}):
-  if link not in links:
+  if link.text and link not in links:
     links.append({
-      "title": link.text,
+      "title": link['title'],
       "url": link['href']
-    })
-
-print(links[0:5])
+    }) 
+    
+with open('wikiData.json', 'w') as outfile:
+  json.dump(links[0:5], outfile)
